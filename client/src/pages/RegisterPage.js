0d1c0a92df73d538e00c5f37451517203f175d7f
ths00 <97080735+sth00619@users.noreply.github.com>
2025-05-23 23:59:07 +0900

    backend swagger 수정 중

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>회원가입</h1>
+      <p>회원가입 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default RegisterPage;
