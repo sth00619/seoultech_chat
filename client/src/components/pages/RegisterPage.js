4b0d1c5500e8f589c28a9c101a9c947d115697ac
ths00 <97080735+sth00619@users.noreply.github.com>
2025-05-24 02:48:32 +0900

    page 제대로 출력할 수 있도록 수정 중

--- client/src/components/pages/RegisterPage.js
+++ client/src/components/pages/RegisterPage.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
-import Layout from '../components/common/Layout';
 import RegisterForm from '../components/auth/RegisterForm';
-import { ROUTES } from '../utils/constants';
+import '../styles/globals.css';
 
 const RegisterPage = () => {
-  const { isAuthenticated, loading } = useAuth();
-
-  if (loading) {
-    return (
-      <Layout>
-        <div className="page-loading">로딩 중...</div>
-      </Layout>
-    );
-  }
-
-  if (isAuthenticated) {
-    return <Navigate to={ROUTES.CHAT} replace />;
-  }
-
   return (
-    <Layout>
-      <div className="auth-page">
-        <RegisterForm />
-      </div>
-    </Layout>
+    <div className="auth-page">
+      <RegisterForm />
+    </div>
   );
 };
 
